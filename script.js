61c441b5a8b463d8ba57fd5a29f9c947cd7e585b
chakrit-jsd <chakritabv@gmail.com>
2023-03-13 13:49:36 +0700

    done

---
+++ script.js
@@ -0,0 +1,28 @@
+const dayNumber = 1 ;// input 
+
+switch (dayNumber){
+    case 0 :
+        console.log('Sunday') 
+        break
+    case 1 :
+        console.log('Monday') 
+        break
+    case 2 :
+        console.log('Tuesday') 
+        break
+    case 3 :
+        console.log('Wednesday') 
+        break
+    case 4 :
+        console.log('Thurday') 
+        break
+    case 5 :
+        console.log('Friday') 
+        break
+    case 06 :
+        console.log('Sunday') 
+        break
+    default :
+        console.log('Invalid day number')
+    
+}
